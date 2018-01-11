9fe2af8d28d02dd643ea4915720832d7c28dbb83
Alan Smith <alan@randomsmiths.com>
2018-01-11 12:22:39 -0500

    attempting to move up a directory

--- src/org/usfirst/frc/team3459/robot/Robot.java
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -12,15 +12,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
+ * Don't change the name of this or it won't work. (The manifest looks for
+ * "Robot")
  */
 public class Robot extends IterativeRobot {
+	/* list of autonomous choices go here */
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
+	/* end of list */
+	
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 
@@ -36,26 +36,46 @@ public class Robot extends IterativeRobot {
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
+	 * This function is called once when we go into the teleop mode
+	 */
+	@Override
+	public void teleopInit() {
+	}
+	
+	/**
+	 * This function is called periodically during operator control (approx 20ms)
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called once when we go into the test mode
+	 */
+	@Override
+	public void testInit() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+	
+	/**
+	 * This function is called once when we go into the Autonomous mode
 	 */
 	@Override
 	public void autonomousInit() {
 		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
+
 		System.out.println("Auto selected: " + m_autoSelected);
 	}
 
 	/**
-	 * This function is called periodically during autonomous.
+	 * This function is called periodically during autonomous control (approx
+	 * 20ms)
 	 */
 	@Override
 	public void autonomousPeriodic() {
@@ -68,19 +88,5 @@ public class Robot extends IterativeRobot {
 				// Put default auto code here
 				break;
 		}
-	}
-
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	@Override
-	public void teleopPeriodic() {
-	}
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
 	}
 }
