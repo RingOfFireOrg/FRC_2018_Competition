690756f7d737ce98d8e381b923ff15f96acaeee5
TFD Member <TFD Member@DESKTOP-1NCUN7S>
2018-02-05 21:15:03 -0500

    made it lift and intake properly

--- src/org/usfirst/frc/team3459/robot/Robot.java
+++ src/org/usfirst/frc/team3459/robot/Robot.java
@@ -7,7 +7,12 @@
 
 package org.usfirst.frc.team3459.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,6 +24,15 @@ public class Robot extends IterativeRobot {
 	/* list of autonomous choices go here */
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
+	TankDrive drive = new TankDrive();
+	
+	TalonSRX intake0 = new TalonSRX(4);
+	TalonSRX intake1 = new TalonSRX(5);
+	Victor lift0 = new Victor(4);
+	Victor lift1 = new Victor(5);
+	
+	Joystick stick0 = new Joystick(0);
+	Joystick stick1 = new Joystick(1);
 	/* end of list */
 	
 	private String m_autoSelected;
@@ -47,6 +61,29 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		drive.tankDrive(-stick1.getY(), -stick0.getY());
+		
+		if (stick0.getRawButton(3)) {
+			lift0.set(1.0);
+			lift1.set(1.0);
+		} else if (stick0.getRawButton(2)) {
+			lift0.set(-0.8);
+			lift1.set(-0.8);
+		} else {
+			lift0.set(0.0);
+			lift1.set(0.0);
+		}
+		
+		if (stick1.getRawButton(3)) {
+			intake0.set(ControlMode.PercentOutput, 1.0);
+			intake1.set(ControlMode.PercentOutput, 1.0);			
+		} else if (stick1.getRawButton(2)) {
+			intake0.set(ControlMode.PercentOutput, -1.0);
+			intake1.set(ControlMode.PercentOutput, -1.0);
+		} else {
+			intake0.set(ControlMode.PercentOutput, 0.0);
+			intake1.set(ControlMode.PercentOutput, 0.0);
+		}
 	}
 
 	/**
