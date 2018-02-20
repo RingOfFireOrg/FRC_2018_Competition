d32aedfd3b1ac20a3b097e28049dd95da1bb7f30
TFD Member <TFD Member@DESKTOP-046GVOD>
2018-02-19 20:24:30 -0500

    added drive train encoders and upadated robot map

--- src/org/usfirst/frc/team3459/robot/RobotMap.java
+++ src/org/usfirst/frc/team3459/robot/RobotMap.java
@@ -34,7 +34,18 @@ public final class RobotMap {
 	public static final int OPEN_SOLENOID = 0;
 	public static final int CLOSE_SOLENOID = 1;
 
-
+	//Drive Train Encoders
+	public static final int DRIVE_TRAIN_LEFT_ENCODER_A = 2;
+	public static final int DRIVE_TRAIN_LEFT_ENCODER_B = 3;
+	public static final int DRIVE_TRAIN_RIGHT_ENCODER_A = 4;
+	public static final int DRIVE_TRAIN_RIGHT_ENCODER_B = 5;
+	
+	//Drive Train Motors 
+	public static final int MOTOR_FRONT_LEFT = 2;
+	public static final int MOTOR_FRONT_RIGHT = 0;
+	public static final int MOTOR_BACK_LEFT = 3;
+	public static final int MOTOR_BACK_RIGHT = 1;
+	
 
 	
 	
--- src/org/usfirst/frc/team3459/robot/TankDrive.java
+++ src/org/usfirst/frc/team3459/robot/TankDrive.java
@@ -1,13 +1,34 @@
 package org.usfirst.frc.team3459.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TankDrive extends DifferentialDrive {
+	
+	private Encoder leftEncoder = new Encoder(RobotMap.DRIVE_TRAIN_LEFT_ENCODER_A, RobotMap.DRIVE_TRAIN_LEFT_ENCODER_B, false, Encoder.EncodingType.k1X);
+	private Encoder rightEncoder = new Encoder(RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_A, RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_B, false, Encoder.EncodingType.k1X);
+	
 	TankDrive() {
-		super(new SpeedControllerGroup(new Victor(2), new Victor(3)),
-				new SpeedControllerGroup(new Victor(0), new Victor(1)));
+		super(new SpeedControllerGroup(new Victor(RobotMap.MOTOR_FRONT_LEFT), new Victor(RobotMap.MOTOR_BACK_LEFT)),
+				new SpeedControllerGroup(new Victor(RobotMap.MOTOR_FRONT_RIGHT), new Victor(RobotMap.MOTOR_BACK_RIGHT)));
+		initEncoder(leftEncoder);
+		initEncoder(rightEncoder);
+	}
+	
+	private void initEncoder(Encoder encoder) {
+		encoder.reset();
+		encoder.setSamplesToAverage(5); // noise reduction?
+		encoder.setDistancePerPulse(1.0/360); // should see 1 pulse per rotation
+	}
+	
+	public void printEncoderValue() {
+		SmartDashboard.putNumber("left Encoder", leftEncoder.get());
+		SmartDashboard.putNumber("left rotations", leftEncoder.getDistance());
+		SmartDashboard.putNumber("right Encoder", rightEncoder.get());
+		SmartDashboard.putNumber("right rotations", rightEncoder.getDistance());
 	}
 
 }
